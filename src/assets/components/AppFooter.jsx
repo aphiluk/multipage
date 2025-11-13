19c6a6cc6a853fc0599d671a33b7b73611db1a26
aphiluk <aphiluktle@gmail.com>
2025-11-14 00:02:35 +0700

    commitdelete

--- src/assets/components/AppFooter.jsx
+++
@@ -1,9 +0,0 @@
-export default function AppFooter() {
-  return (
-    <footer className="app-footer">
-      <p>มหาวิทยาลัยศรีปทุม</p>
-      <p>คณะเทคโนโลยีสารสนเทศ | สาขาวิทยาการคอมพิวเตอร์และนวัตกรรมการพัฒนาซอฟแวร์</p>
-      <p>Facebook : Aphiluk | Instagram : aplpp.t | Line : aphiluk.p</p>
-    </footer>
-  );
-}
--- src/assets/components/AppHeader.jsx
+++
@@ -1,7 +0,0 @@
-export default function AppHeader() {
-  return (
-    <header className="app-header">
-      <h1>CSI-205 การพัฒนาโปรแกรมส่วนหน้า</h1>
-    </header>
-  );
-}
